d4921de65954582f6e639e920ed62c40c245f160
Chris Farmiloe <chrisfarms@gmail.com>
2014-02-01 13:04:13 +0000

    init

---
+++ lib/vagrant/nixos.rb
@@ -0,0 +1,7 @@
+require "vagrant/nixos/version"
+
+module Vagrant
+  module Nixos
+    # Your code goes here...
+  end
+end
---
+++ lib/vagrant/nixos/version.rb
@@ -0,0 +1,5 @@
+module Vagrant
+  module Nixos
+    VERSION = "0.0.1"
+  end
+end
